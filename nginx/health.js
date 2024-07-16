7f122494dc28bdb52b3f4088e767948ec83541fd
Pavel Komov <kmvpvl@gmail.com>
2024-07-16 18:30:56 +0300

    check: JSON return trying

--- nginx/health.js
+++ nginx/health.js
@@ -1,5 +1,10 @@
-function hello(r) {
-    r.return(200, "Hello world!\n");
+function health(r) {
+    r.return(200, {
+        "overal_status": "OK",
+        "api": {
+            "version": "1.0.0"
+        }
+    });
   }
   
   export default {hello}
